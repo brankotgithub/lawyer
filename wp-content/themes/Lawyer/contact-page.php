95d7dba6619fdb1d6e2d28685edd0be64ab2d88a
Branko <teodorovic.b@gmail.com>
2023-10-08 12:13:08 +0200

    Custom theme Lawyer - Create Contact Form, Configure Mail Server for Contact Form, Add Google Map and Page 404

--- wp-content/themes/Lawyer/contact-page.php
+++ wp-content/themes/Lawyer/contact-page.php
@@ -32,24 +32,11 @@ get_header();
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-10 col-lg-6">
-                    <h2 class="text-center mb-5">Please contact us using the form bellow</h2>
-
-                    <form class="contact-form" action="" method="get">
-                        <div class='form-group'>
-                            <input type="text" name="name" value="" class="form-control" placeholder='Name and surname *'> 
-                            <div class="error-msg"></div>
-                        </div>
-                        <div class='form-group'>
-                            <input type="email" name="email" value="" class="form-control" placeholder='Email address *'> 
-                            <div class="error-msg"></div>
-                        </div>
-                        <div class='form-group'>
-                            <textarea name="message" placeholder="Enter your message *" class="form-control" rows='7'></textarea>
-                            <div class="error-msg"></div>
-                        </div>
-                        <div class="form-group text-center">
-                            <button class="btn btn-primary px-5 py-3 rounded-pill text-uppercase" type="submit" name="send" value="true">send message</button>
-                        </div>
+                    <h2 class="text-center mb-5"><?php the_field('contact_form_title'); ?></h2>
+                   
+                    
+                    <form class="contact-form" action="" method="post">
+                        <?php the_field('contact_page_form'); ?> 
                     </form>
                 </div>
             </div>
@@ -58,7 +45,7 @@ get_header();
 
     <section class="contact-map">
         <div class="embed-responsive embed-responsive-21by9">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d16018.572327114758!2d20.44038030784272!3d44.78671155305784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2srs!4v1569605872608!5m2!1sen!2srs" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            <?php the_field('contact_page_map'); ?>
         </div>
     </section>
 </main>
